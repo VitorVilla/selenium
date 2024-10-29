cdbc0d645a323bbdc3a8b7d1cd33c696fbb605da
Vitor Oliveira <vivilla1010@gmail.com>
2024-10-29 19:45:05 -0300

    novas funcionalidades

--- src/main/java/org/example/GoogleSearchExample.java
+++ src/main/java/org/example/GoogleSearchExample.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class GoogleSearchExample {
     public static void main(String[] args) {
 
+        int err = 0;
+
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -24,16 +26,40 @@ public class GoogleSearchExample {
             // Envia o formulário
             searchBox.submit();
 
-            // Espera alguns segundos para a página carregar (não recomendado em produção)
-            Thread.sleep(2000);
+            // Espera alguns segundos para a página carregar
+            Thread.sleep(5000);
 
             // Obtém e exibe o título da página de resultados
             System.out.println("Título da página: " + chrome.getTitle());
+
+            // Limpa o campo de busca e realiza a segunda pesquisa
+            searchBox = chrome.findElement(By.name("q")); // Atualiza o campo de busca
+            searchBox.clear();
+            searchBox.sendKeys("Selenium");
+            searchBox.submit();
+
+            // Espera novamente para a página de resultados carregar
+            Thread.sleep(5000);
+
+            // Clica no link do site Selenium na página de resultados
+            WebElement seleniumLink = chrome.findElement(By.partialLinkText("selenium.dev"));
+            seleniumLink.click();
+
+            // Aguarda o carregamento do site
+            Thread.sleep(5000);
+
         } catch (InterruptedException e) {
+            err = 1;
             e.printStackTrace();
         } finally {
             // Fecha o navegador
-            System.out.println("Deu bom");
+            if (err == 0) {
+                System.out.println("Sucesso!");
+                chrome.quit();
+            } else {
+                System.out.println("Erro!");
+            }
+
         }
     }
 }
