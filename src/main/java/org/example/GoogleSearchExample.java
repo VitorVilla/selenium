2f6e9f9163c2c8b45cdf8f2e109064447b3312a6
Vitor Oliveira <vivilla1010@gmail.com>
2024-10-29 13:31:32 -0300

    Ajuste na classe

--- src/main/java/org/example/GoogleSearchExample.java
+++ src/main/java/org/example/GoogleSearchExample.java
@@ -1,23 +1,25 @@
 package org.example;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleSearchExample {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        WebDriver chrome = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver chrome = new ChromeDriver(options);
 
         try {
             // Navega até o Google
-            chrome.get("https://link.devaprender.com");
+            chrome.get("https://www.google.com.br");
 
             // Encontra o campo de busca e insere o termo
             WebElement searchBox = chrome.findElement(By.name("q"));
-            searchBox.sendKeys("ChromeDriver");
+            searchBox.sendKeys("Melhor aula de terça");
 
             // Envia o formulário
             searchBox.submit();
@@ -31,7 +33,7 @@ public class GoogleSearchExample {
             e.printStackTrace();
         } finally {
             // Fecha o navegador
-            chrome.quit();
+            System.out.println("Deu bom");
         }
     }
 }
